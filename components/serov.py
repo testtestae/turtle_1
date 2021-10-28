0bf98c1c7ac452eff08460858e30a8785bc068be
Your Name <you@example.com>
2021-10-28 16:01:15 +0500

    serov.py

--- components/serov.py
+++ components/serov.py
@@ -1,15 +1,31 @@
 import turtle
-
 t = turtle.Turtle()
 s = turtle.Screen()
-t.goto(250,250)
+
+t.fd(100)
+t.rt(90)
+t.fd(100)
 t.lt(90)
+t.fd(100)
+t.rt(90)
+t.fd(100)
 t.lt(90)
-t.fd(500)
-t.lt(135)
-t.fd(550)
+t.fd(100)
+t.rt(90)
+t.fd(100)
+t.rt(90)
+t.fd(100)
 t.lt(90)
-t.lt(60)
-t.fd(550)
+t.fd(100)
+t.rt(90)
+t.fd(100)
 t.lt(90)
-t.
+t.fd(100)
+t.rt(90)
+t.fd(100)
+t.rt(90)
+
+
+
+
+
